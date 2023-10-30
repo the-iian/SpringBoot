4e6737fcde30da7f39abd9381b9a245ea9205bc8
iian <yiaahn@gmail.com>
2023-10-30 17:48:11 +0900

    Delete API

---
+++ src/main/java/com/hello/spring/controller/DeleteApiController.java
@@ -0,0 +1,19 @@
+package com.hello.spring.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteApiController {
+
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account){
+
+        System.out.println(userId);
+        System.out.println(account);
+
+        // delete 자체가 리소스 삭제이다.
+        // 에러가 나더라도 에러대신 200ok를 던진다
+
+    }
+}
